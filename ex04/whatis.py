38832f4c9f1bd0f129b9bcd2ed9bd7af567a95f1
Lucas <lucas@MacBook-Air-de-Lucas.local>
2023-08-30 18:15:03 +0200

    In progress

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,9 +1,16 @@
 import sys
 
-if sys.argv[1] % 2 == 0:
-	print("I'm Even.")
+def check_even_odd(number):
+	num = int(number)
 
-if sys.argv[1] % 2 != 0:
-	print("I'm Odd.")
+	if num % 2 == 0:
+		print("I'm Even.")
+	else:
+		print("I'm Odd.")
 
+if len(sys.argv) == 2:
+	assert sys.argv[1].lstrip("-").isdigit(), "argument is not an integer"
+	check_even_odd(sys.argv[1])
 
+elif len(sys.argv) > 2:
+	assert False, "more than one argument is provided"
